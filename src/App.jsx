5bb2ba642e521a8249447ef910a2977dddb1297c
Reagan Duggins <7460851+ReaganDuggins@users.noreply.github.com>
2025-09-10 13:52:00 -0400

    [f] displays the phrase in the selected language.

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,56 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import Phrases from './assets/phrases.json' assert { type: 'json' };
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [curLanguage, setCurLanguage] = useState("Hebrew")
+
+  const showPhrase = () => {
+    const data = Phrases[0];
+    console.log(Object.keys(data[curLanguage]));
+
+    return (
+      <section>
+        <section>
+          <label>English: </label>
+          <span>{data.English}</span>
+        </section>
+        {
+          data[curLanguage].map((langKind) => {
+            return (
+              <section>
+                <section>
+                  <label>{curLanguage} {langKind.Kind}: </label>
+                  <span>{langKind.Native}</span>
+                </section>
+                <section>
+                  <label>Pronunciation: </label>
+                  <span>{langKind.Pronunciation}</span>
+                </section>
+              </section>
+            );
+          })
+        }
+        
+        
+      </section>
+    )
+  }
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      
+        <h1>Phrase of the Day</h1>
+        <select onChange={(e) => {setCurLanguage(e.target.value)}}>
+          <option value="Hebrew">Hebrew</option>
+          <option value="Old Norse">Old Norse</option>
+          <option value="Old English">Old English</option>
+        </select>
+
+        <section>
+          <h2>{curLanguage}</h2>
+          {showPhrase()}
+        </section>
     </>
   )
 }
